9f1076b98f2d459d72a2b3ac19748843c2f0f80b
mikhaeltatischev <tatischevm@gmail.com>
2023-03-31 23:39:22 +0300

    style: удаление ненужных импортов

--- src/test/java/ru/yandex/practicum/filmorate/controllerTest/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllerTest/UserControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.controller.UserController;
 
-import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
